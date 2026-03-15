f353a83176008b9cc4dbe677d6d06a93869d2649
famadorgwyn <famador.gwyn.schwann@gmail.com>
2026-03-15 15:08:07 +0800

    Create session1.js

---
+++ session1.js
@@ -0,0 +1,10 @@
+<h2>Session 1: Introduction to JavaScript</h2>
+
+<p><b>Discussion:</b></p>
+<p>JavaScript is a programming language used to make websites interactive.</p>
+<p>It allows websites to respond to user actions like clicking buttons.</p>
+<p>HTML creates the structure, CSS designs the style, and JavaScript adds behavior.</p>
+
+<script>
+alert("Welcome to JavaScript Programming!");
+</script>
